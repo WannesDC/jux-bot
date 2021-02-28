be3ab47d416a81491d232692c5fbcf6cb1602d81
Wannes De Craene <wannesdecraene@hotmail.com>
2021-02-28 11:21:08 +0100

    added table

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,6 +1,23 @@
+-- Version 1.0.0
 CREATE TABLE IF NOT EXISTS users (
         player_id varchar,
         player_name varchar,
         api_key varchar,
         discord_user_id varchar
 );
+
+-- Version 1.1.0
+ALTER TABLE IF EXISTS users ADD CONSTRAINT users_pk PRIMARY KEY (discord_user_id);
+
+CREATE TABLE IF NOT EXISTS role (
+        role_id varchar,
+        role_name varchar,
+        server_id varchar,
+        constraint role_pk primary key (role_id)
+);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+        discord_user_id varchar,
+        role_id varchar
+);
+
